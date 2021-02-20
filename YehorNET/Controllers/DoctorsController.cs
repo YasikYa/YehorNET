fc51a8c8fea44d16b8698be5527d71ef23de7e10
yaros.vodotyka@gmail.com <yaros.vodotyka@gmail.com>
2021-02-20 22:47:25 +0200

    add details action

--- YehorNET/Controllers/DoctorsController.cs
+++ YehorNET/Controllers/DoctorsController.cs
@@ -14,19 +14,53 @@ namespace YehorNET.Controllers
 
         public DoctorsController(AppDbContext dbContext) => _dbContext = dbContext;
 
-        public IActionResult Search(string searchString)
+        public IActionResult List(string searchString, string[] treatments)
         {
-            var matches = _dbContext.Doctors.Where(d => d.Name.Contains(searchString))
-                .Select(d => new DoctorsListItemViewModel
+            var selectQuery = _dbContext.Doctors.AsQueryable();
+
+            if (!string.IsNullOrEmpty(searchString))
+                selectQuery = selectQuery.Where(d => d.Name.Contains(searchString));
+            if (treatments != null && treatments.Length != 0)
+                selectQuery = selectQuery.Where(d => d.Treatments.Any(t => treatments.Any(filterTreatment => filterTreatment.Equals(t))));
+
+            var result = selectQuery.Select(d => new DoctorsListItemViewModel
+            {
+                Id = d.Id,
+                Name = d.Name,
+                Rate = d.Comments.Count == 0 ? 0 : d.Comments.Select(c => c.Rate).Average(),
+                CommentsCount = d.Comments.Count,
+                Treats = d.Treatments.Select(tb => tb.Name).ToList()
+            }).ToList();
+
+            return View(result);
+        }
+
+        public IActionResult Details(Guid id)
+        {
+            var detailsModel = _dbContext.Doctors.Where(d => d.Id == id)
+                .Select(d => new DoctorDetailsViewModel
                 {
                     Id = d.Id,
                     Name = d.Name,
-                    //Rate = d.Comments.Select(c => c.Rate).Average(),
-                    CommentsCount = d.Comments.Count,
-                    //Treats = d.TreatmentBranches.Select(tb => tb.Name).ToList()
-                }).ToList();
+                    Age = d.Age,
+                    ContactPhone = d.ContactPhone,
+                    Rating = d.Comments.Count == 0 ? 0 : d.Comments.Select(c => c.Rate).Average(),
+                    ProffecionalExperienceFrom = d.ProffecionalExperienceFrom,
+                    VisitPrice = d.VisitPrice,
+                    Clinic = new ClinicViewModel
+                    {
+                        Id = d.Clinic.Id,
+                        Name = d.Clinic.Title,
+                        SiteUrl = d.Clinic.WebsiteUrl
+                    },
+                    Comments = d.Comments.Select(c => new CommentViewModel
+                    {
+                        Rate = c.Rate,
+                        Text = c.Comments
+                    }).ToList()
+                }).FirstOrDefault();
 
-            return View(matches);
+            return View(detailsModel);
         }
     }
 }
---
+++ YehorNET/Models/DoctorDetailsViewModel.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace YehorNET.Models
+{
+    public class DoctorDetailsViewModel
+    {
+        public Guid Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string ContactPhone { get; set; }
+
+        public int Age { get; set; }
+
+        public int VisitPrice { get; set; }
+
+        public double Rating { get; set; }
+
+        public DateTime ProffecionalExperienceFrom { get; set; }
+
+        public ClinicViewModel Clinic { get; set; }
+
+        public ICollection<CommentViewModel> Comments { get; set; }
+    }
+
+    public class CommentViewModel
+    {
+        public double Rate { get; set; }
+
+        public string Text { get; set; }
+    }
+
+    public class ClinicViewModel
+    {
+        public Guid Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string SiteUrl { get; set; }
+    }
+}
