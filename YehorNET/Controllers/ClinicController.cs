daf3cabbd6c0413070e261aa94e2729a2699fa3d
yaros.vodotyka@gmail.com <yaros.vodotyka@gmail.com>
2021-02-22 12:19:39 +0200

    add create clinic

---
+++ YehorNET/Controllers/ClinicController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using YehorNET.DAL;
+using YehorNET.DAL.Domain;
+using YehorNET.Models;
+
+namespace YehorNET.Controllers
+{
+    public class ClinicController : Controller
+    {
+        private readonly AppDbContext _dbContext;
+
+        public ClinicController(AppDbContext dbContext) => _dbContext = dbContext;
+
+        [HttpGet]
+        public IActionResult Create(string callbackUrl)
+        {
+            ViewBag.ReturnUrl = callbackUrl;
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(CreateClinicViewModel model, string callbackUrl)
+        {
+            _dbContext.Clinics.Add(new Clinic
+            {
+                Title = model.Name,
+                Address = model.Address,
+                ContactNumber = model.ContactPhone,
+                WebsiteUrl = model.SiteUrl
+            });
+            _dbContext.SaveChanges();
+            return string.IsNullOrEmpty(callbackUrl) ? RedirectToAction("Index", "Home") : (IActionResult)Redirect(callbackUrl);
+        }
+    }
+}
---
+++ YehorNET/Models/CreateClinicViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace YehorNET.Models
+{
+    public class CreateClinicViewModel
+    {
+        public string Name { get; set; }
+
+        public string ContactPhone { get; set; }
+
+        public string Address { get; set; }
+
+        public string SiteUrl { get; set; }
+    }
+}
